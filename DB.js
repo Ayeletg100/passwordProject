7a8124af72f286c5dadfa12ee3c9bdaabddfd413
Ayeletg100 <ayeletginsberg@hilma.tech>
2023-11-01 11:43:26 +0200

    things

--- DB.js
+++ DB.js
@@ -1,8 +1,39 @@
 class DB{
     constructor(){
-        this.users=[];
+        
     }
+
+    static createDB(){
+        const user= new User('ani1@gmail.com', '12345678');
+        localStorage.setItem('users', JSON.stringify([user]));
+
+    }
+
+    get users(){
+        return localStorage.getItem();
+    }
+
+     getWebPasswords(id) {
+        for(user in users){
+            if(user.id===id){
+                return user.getWebPasswords;
+            }
+
+        }
+        return false;
+    }
+
+    getSpecificWebPassword(idUser,idWebPassword){
+        currentWebPasswordLIST= getWebPasswords(idUser);
+        if(currentWebPasswordLIST){
+            for(wp in currentWebPasswordLIST){
+                if(wp.id===idWebPassword){
+                    return wp;
+                }
     
+            }
+        }
+    }
 }
 
 
