7071fe838995f6bfe449cbae5a83dcc967a7fb53
EfratElimelech2502 <efratelimelech@hilma.tech>
2023-11-01 18:24:47 +0200

    fajax class added

--- fajax.js
+++ fajax.js
@@ -1,3 +1,12 @@
-class FAJAX{
-
+class FXMLHttpRequest {
+    open(method, url) {
+        this.method = method;
+        this.url = url;
+    }
+    send(obj) {
+        this.obj = obj;
+        const response = figure(this.method, this.url, this.obj);
+        this.response = response;
+        this.onload();
+    }
 }
