840290003c7eb62faf67d1a145b18cfda794a035
EfratElimelech2502 <efratelimelech@hilma.tech>
2023-11-01 14:35:19 +0200

    open password list func and corrected things in classes

--- server.js
+++ server.js
@@ -1,49 +1,67 @@
-class User{
-    static count= 1;
+class User {
+    static count = 1;
 
-    constructor(username, password ){
-        this.id=User.count;
-        this._username=username;
-        this._password=password;
-        this._webPasswords=[];
+    constructor(username, password) {
+        this._id = User.count;
+        this._username = username;
+        this._password = password;
+        this._webPasswords = [new webPassword("google" , "e", "123")];
         User.count++;
     }
 
-    get username(){
+//doesn't work
+    get GetUsername() {
+        return (this._username);
+    }
 
+    get GetPassword() {
+        return (this._password);
+    }
+
+    get webPasswords(){
+
+        return this._webPasswords;
     }
 }
 
+class webPassword {
+    static count = 1;
 
-
-class webPassword{
-    static count= 1;
-
-    constructor(webName, userNameW,passwordW ){
-        this.id= webPassword.count;
-        this._webName=webName;
-        this._userNameW=userNameW;
-        this._passwordW=passwordW;
+    constructor(webName, userNameW, passwordW) {
+        this._id = webPassword.count;
+        this._webName = webName;
+        this._userNameW = userNameW;
+        this._passwordW = passwordW;
         webPassword.count++;
     }
-    get id(){
+    get id() {
         return this.id;
     }
 
-    get userNameW(){
+    get userNameW() {
         return this._userNameW;
     }
 
-    get password(){
+    get password() {
         return this._passwordW;
     }
-
-    set userNameW(userNameW){
-        this._userNameW=userNameW;
+    get webName(){
+        return this._webName
     }
 
-    set passwordW(passwordW){
-        this.passwordW=passwordW;
+    set userNameW(userNameW) {
+        this._userNameW = userNameW;
+    }
+
+    set passwordW(passwordW) {
+        this._passwordW = passwordW;
     }
 }
 
+function getPasswordList(user) {
+    console.log("server");
+    //ask db
+    const passwordsList = getWebPasswords(user.id);
+    console.log('passwordsList: ', passwordsList);
+    return passwordsList;
+}
